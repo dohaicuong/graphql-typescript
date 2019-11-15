8d469162a7996782085b079597ab8f7b55c1e8b6
dohaicuong <beatyshot@gmail.com>
2019-11-15 13:35:29 +1000

    adding validation args for mutation middleware

--- src/models/User/signup.resolvers.ts
+++ src/models/User/signup.resolvers.ts
@@ -1,13 +1,22 @@
 import { MutationResolvers, AuthPayload } from '../graphql_types'
+import * as yup from 'yup'
 
-const Mutation: MutationResolvers = {
-  signup: (): AuthPayload => {
-    return {
-      token: '123',
-      user: {
-        id: '1',
-        email: 'beatyshot@gmail.com',
-        name: 'Yuki',
+const Mutation = {
+  signup: {
+    validate: yup.object().shape({
+      data: yup.object().shape({
+        email: yup.string().required().email(),
+        password: yup.string().required()
+      })
+    }),
+    resolve: (_, args): AuthPayload => {
+      return {
+        token: '123',
+        user: {
+          id: '1',
+          email: 'beatyshot@gmail.com',
+          name: 'Yuki',
+        }
       }
     }
   }
